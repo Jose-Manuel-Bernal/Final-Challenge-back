13e35d1ddd37a9f08f54346e7b340531ad400f53
Jose Manuel Bernal Aguilar <josem.bernal@udea.edu.co>
2022-06-10 02:55:14 -0500

    first commit, project creation

---
+++ src/main/java/com/sofkau/finallChallenge/FinallChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.sofkau.finallChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinallChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinallChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofkau/finallChallenge/FinallChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkau.finallChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinallChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
