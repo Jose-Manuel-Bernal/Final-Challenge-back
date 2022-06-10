e4a68aad0c454ecbd4fe0bcc8796be5a4ef95586
Jose Manuel Bernal Aguilar <josem.bernal@udea.edu.co>
2022-06-10 04:10:01 -0500

    model paer and web flux config added

---
+++ src/main/java/com/sofkau/finallChallenge/entity/Bill.java
@@ -0,0 +1,4 @@
+package com.sofkau.finallChallenge.entity;
+
+public class Bill {
+}
---
+++ src/main/java/com/sofkau/finallChallenge/entity/Provider.java
@@ -0,0 +1,14 @@
+package com.sofkau.finallChallenge.entity;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "persons")
+public class Provider {
+
+    @Id
+    private String id;
+    private String name;
+    private String phoneNumber;}
