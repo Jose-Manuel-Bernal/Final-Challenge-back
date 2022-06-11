e6d9a094b76729a05c4420b9052a345053a61e1d
Jose Manuel Bernal Aguilar <josem.bernal@udea.edu.co>
2022-06-11 06:17:38 -0500

    swagger for product routes added

--- src/main/java/com/sofkau/finallChallenge/routes/product/DeleteProductRoute.java
+++ src/main/java/com/sofkau/finallChallenge/routes/product/DeleteProductRoute.java
@@ -1,10 +1,17 @@
 package com.sofkau.finallChallenge.routes.product;
 
 import com.sofkau.finallChallenge.usecases.product.DeleteProductUseCAse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
@@ -16,6 +23,15 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 public class DeleteProductRoute {
 
     @Bean
+    @RouterOperation(path = "/delete/product/{id}", produces = {
+            MediaType.APPLICATION_JSON_VALUE}, method = RequestMethod.DELETE, beanClass = DeleteProductUseCAse.class, beanMethod = "apply"
+            , operation = @Operation(operationId = "deleteProduct", responses = {
+            @ApiResponse(responseCode = "200", description = "successful operation"),
+            @ApiResponse(responseCode = "400", description = "Invalid Product ID supplied"),
+            @ApiResponse(responseCode = "404", description = "Product not found")}, parameters = {
+            @Parameter(in = ParameterIn.PATH, name = "id")}
+    ))
+
     public RouterFunction<ServerResponse> deleteProduct (DeleteProductUseCAse useCAse) {
         return route(DELETE("/delete/product/{id}").and(accept(MediaType.APPLICATION_JSON)),
                 request -> useCAse.apply(request.pathVariable("id"))
--- src/main/java/com/sofkau/finallChallenge/routes/product/PutProductRoute.java
+++ src/main/java/com/sofkau/finallChallenge/routes/product/PutProductRoute.java
@@ -1,11 +1,21 @@
 package com.sofkau.finallChallenge.routes.product;
 
 import com.sofkau.finallChallenge.dto.ProductDTO;
+import com.sofkau.finallChallenge.entity.Product;
 import com.sofkau.finallChallenge.usecases.product.PutProductUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
@@ -18,6 +28,16 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 public class PutProductRoute {
 
     @Bean
+    @RouterOperation(path = "/put/product"
+            , produces = {
+            MediaType.APPLICATION_JSON_VALUE}, method = RequestMethod.PUT, beanClass = PutProductUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "putProduct", responses = {
+                    @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = Product.class))),
+                    @ApiResponse(responseCode = "400", description = "Invalid Product ID supplied"),
+                    @ApiResponse(responseCode = "404", description = "Product not found")}
+                    , requestBody = @RequestBody(content = @Content(schema = @Schema(implementation = Product.class))))
+    )
+
     public RouterFunction<ServerResponse> putProduct (PutProductUseCase useCase) {
         return route(PUT("/put/product").and(accept(MediaType.APPLICATION_JSON)),
                 request -> request.bodyToMono(ProductDTO.class)
