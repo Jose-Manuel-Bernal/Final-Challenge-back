abea25d676bb1b34cc3f76a32e962d8ea79e341e
Jose Manuel Bernal Aguilar <josem.bernal@udea.edu.co>
2022-06-10 22:34:01 -0500

    swagger dependencies added and tested

--- src/main/java/com/sofkau/finallChallenge/routes/product/GetProductsRoute.java
+++ src/main/java/com/sofkau/finallChallenge/routes/product/GetProductsRoute.java
@@ -1,11 +1,20 @@
 package com.sofkau.finallChallenge.routes.product;
 
 import com.sofkau.finallChallenge.dto.ProductDTO;
+import com.sofkau.finallChallenge.entity.Product;
 import com.sofkau.finallChallenge.usecases.product.GetAllProductsUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -17,6 +26,14 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 public class GetProductsRoute {
 
     @Bean
+    @RouterOperation(path = "/get/products", produces = {
+            MediaType.APPLICATION_JSON_VALUE},
+            beanClass = GetAllProductsUseCase.class, method = RequestMethod.GET, beanMethod = "apply",
+            operation = @Operation(operationId = "geAllProducts", responses = {
+                    @ApiResponse(responseCode = "200", description = "successful operation",
+                            content = @Content(schema = @Schema(implementation = Product.class))),
+                    @ApiResponse(responseCode = "400", description = "Invalid Products details supplied")}
+            ))
 
     public RouterFunction<ServerResponse> getAllProducts (GetAllProductsUseCase useCase) {
         return route(GET("/get/products"),
--- src/main/java/com/sofkau/finallChallenge/routes/product/PostProductRoute.java
+++ src/main/java/com/sofkau/finallChallenge/routes/product/PostProductRoute.java
@@ -1,11 +1,19 @@
 package com.sofkau.finallChallenge.routes.product;
 
 import com.sofkau.finallChallenge.dto.ProductDTO;
+import com.sofkau.finallChallenge.entity.Product;
 import com.sofkau.finallChallenge.usecases.product.PostProductUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
@@ -17,6 +25,14 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 public class PostProductRoute {
 
     @Bean
+    @RouterOperation(path = "/post/product", produces = {
+            MediaType.APPLICATION_JSON_VALUE}, method = RequestMethod.POST, beanClass = PostProductUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "postProduct", responses = {
+                    @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = Product.class))),
+                    @ApiResponse(responseCode = "400", description = "Invalid Product details supplied")}
+                    , requestBody = @RequestBody(content = @Content(schema = @Schema(implementation = Product.class)))
+            ))
+
     public RouterFunction<ServerResponse> postProduct (PostProductUseCase useCase) {
         return route(POST("/post/product").and(accept(MediaType.APPLICATION_JSON)),
                 request -> request.bodyToMono(ProductDTO.class)
