2ea86b390c0cf4ae9ad9e77d2097f0a6c96d2c6f
Jose Manuel Bernal Aguilar <josem.bernal@udea.edu.co>
2022-06-17 09:04:30 -0500

    set date from backend added

--- src/main/java/com/sofkau/finallChallenge/usecases/receipt/PostReceiptUseCase.java
+++ src/main/java/com/sofkau/finallChallenge/usecases/receipt/PostReceiptUseCase.java
@@ -6,6 +6,8 @@ import com.sofkau.finallChallenge.repository.ReceiptRepository;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.time.LocalDate;
+
 @Service
 public class PostReceiptUseCase {
     private ReceiptRepository repository;
@@ -17,6 +19,7 @@ public class PostReceiptUseCase {
     }
 
     public Mono<ReceiptDTO> apply (ReceiptDTO receiptDTO) {
+        receiptDTO.setDate(LocalDate.now());
         return repository.save(mapper.toReceiptEntity(receiptDTO)).map(receipt -> mapper.toReceiptDTO(receipt));
     }
 }
